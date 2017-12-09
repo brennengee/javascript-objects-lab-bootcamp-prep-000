9805030e2178e02694c428c42f95f8ed5c5cfd87
Brennen Gee <brennengee@me.com>
2017-12-09 04:06:25 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+var recipes = new Object({ chia: '1 cup', hemp: '2 cups', zinc: '3 cups' });
+
+function updateObjectWithKeyAndValue(recipes, kale, chopped) {
+  return Object.assign({}, recipes, { [kale]: chopped })
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(recipes, butter, stick) {
+    recipes[butter] = stick;
+    return recipes
+}
+
+function deleteFromObjectByKey(recipes, chia) {
+  delete recipes[chia];
+  return recipes
+}
+
+//////spread or objective.assign, modify the clone instead of the original
